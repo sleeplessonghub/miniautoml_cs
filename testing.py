535ecc15e75a7899f2804deb60fc2bb1367fe7d3
sleeplessonghub <ra4.sleepless@gmail.com>
2026-01-14 13:54:34 +0700

    Update testing.py

--- testing.py
+++ testing.py
@@ -630,7 +630,7 @@ if st.session_state['df_pp'] is not None:
                                                                                 width = None,
                                                                                 autosize = False,
                                                                                 title_x = 0.5,
-                                                                                margin = dict(l = 50)).update_yaxes(title = None)
+                                                                                yaxis_title = None)
             st.plotly_chart(pdp_fig_ss, width = 'stretch', config = {'displayModeBar': False})
           
           elif is_object == True: # Classification modeling
