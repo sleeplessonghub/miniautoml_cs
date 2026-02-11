80b7fa008cfb590158d35007813510974c5e5db3
sleeplessonghub <ra4.sleepless@gmail.com>
2026-02-11 13:25:40 +0700

    Update testing.py

--- testing.py
+++ testing.py
@@ -358,7 +358,10 @@ if st.session_state['df_pp'] is not None:
         st.write('Target Variable Selection (Scrollable):')
       train_info = pd.DataFrame({'Variables': train.columns, 'Non-Null Count': train.count(numeric_only = False), 'Data Type': train.dtypes}).reset_index(drop = True)
       train_info['Data Type'] = train_info['Data Type'].astype(str).map({'float64': 'Numerical', 'object': 'Categorical'})
-      st.dataframe(train_info.astype(str), height = 213 if len(train.columns) > 5 else 'auto', hide_index = True)
+      st.dataframe(train_info.astype(str), height = 213 if len(train.columns) > 5 else 'auto', hide_index = True,
+                   column_config = {'Variables': st.column_config.Column(width = 200),
+                                    'Non-Null Count': st.column_config.Column(width = 200),
+                                    'Data Type': st.column_config.Column(width = 200)})
       unassigned_count_2 = 0
       target = None
       target_class = None
